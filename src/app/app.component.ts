5a1a58305ec5cf5eaba3e927109280aa0165b199
Michael Small <michael.small@relationshipone.com>
2025-01-23 11:35:55 -0600

    chore: create basic web component with @angular/elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {Component, Injector} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
+import {PopupService} from './popup.service';
+import { PopupComponent } from './popup/popup.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <a href="https://angular.dev/guide/elements#limitations" target="_blank">example from docs in a new CLI project with latest 19.1.3 framework 19.1.4 cli patches</a>
+    <!-- <button type="button" (click)="popup.showAsComponent(input.value)">Show as component</button>
+    <button type="button" (click)="popup.showAsElement(input.value)">Show as element</button> -->
+    <my-popup message="hey" />
+  `,
+  providers: [PopupService],
+  imports: [PopupComponent],
 })
 export class AppComponent {
-  title = 'elements-hmr-repro';
+  constructor(
+    injector: Injector,
+    public popup: PopupService,
+  ) {
+    // Convert `PopupComponent` to a custom element.
+    const PopupElement = createCustomElement(PopupComponent, {injector});
+    // Register the custom element with the browser.
+    customElements.define('popup-element', PopupElement);
+  }
 }
